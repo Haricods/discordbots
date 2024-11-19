7ef92bcbc678f36d11cde3c6ea15224b07e0bd06
Harikrishnan <hk678official668@gmail.com>
2024-11-19 14:56:36 +0000

    now the bot tells joke in discord for " !joke "

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 import os
 from discord.ext import commands
 from discord import Intents, message
+import requests
+import json
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,6 +15,14 @@ client = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
   print("Bot is ready to rock as {0.user}".format(client))
 
+async def Tell_joke():
+  jokes = requests.get("https://official-joke-api.appspot.com/random_joke")
+  json_data = json.loads(jokes.text)
+  ques = json_data["setup"]
+  ans = json_data["punchline"]
+
+  return (ques +" "+ " -" + ans )
+
 
 @client.event
 async def on_message(message):
@@ -20,7 +30,12 @@ async def on_message(message):
     return
   if message.content.startswith("!hello"):
     await message.channel.send("Hello Friend!")
+  joking = await Tell_joke()
+  if message.content.startswith("!joke"):
+    await message.channel.send(joking)
 
+
+  
 
 my_secret = os.environ['TOKEN']
 client.run(my_secret)
