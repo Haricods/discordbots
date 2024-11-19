a844fb0f00115661d231b7b253e35525ab1d1488
Harikrishnan <hk678official668@gmail.com>
2024-11-19 14:16:13 +0000

    got an error running the message intent ,fixed by making some changes

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import discord
 import os
-from discord.ext import commands 
-from discord import Intents
-intents = discord.Intents.default()
+from discord.ext import commands
+from discord import Intents, message
 
-client = commands.Bot(command_prefix=".",intents=intents)
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot(command_prefix="!", intents=intents)
+
 
 @client.event
 async def on_ready():
   print("Bot is ready to rock as {0.user}".format(client))
-  
+
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  if message.content.startswith("!hello"):
+    await message.channel.send("Hello Friend!")
+
+
 my_secret = os.environ['TOKEN']
 client.run(my_secret)
