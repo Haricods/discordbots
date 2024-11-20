202679233e45b7b76f03fb30c480221da4581dd6
Harikrishnan <hk678official668@gmail.com>
2024-11-20 15:00:36 +0000

    uptime robot setup and added creator msg and few other comms too

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ from discord.ext import commands
 from discord import Intents, message
 import requests
 import json
+from keep_alive import keep_alive
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = commands.Bot(command_prefix="!", intents=intents)
 
+comms = ["!hello", "!creator", "!joke", "!help", "!commands"]
+
 
 @client.event
 async def on_ready():
   print("Bot is ready to rock as {0.user}".format(client))
+
 
 async def Tell_joke():
   jokes = requests.get("https://official-joke-api.appspot.com/random_joke")
@@ -21,7 +25,12 @@ async def Tell_joke():
   ques = json_data["setup"]
   ans = json_data["punchline"]
 
-  return (ques +" "+ " -" + ans )
+  return (ques + " " + " -" + ans)
+
+
+def comms_func():
+  for comms in range(4):
+    print(comms)
 
 
 @client.event
@@ -35,8 +44,12 @@ async def on_message(message):
     await message.channel.send(joking)
   if message.content.startswith("!help"):
     await message.channel.send("I tell jokes -start typing '!joke'")
+  if message.content.startswith("!bye"):
+    await message.channel.send("Bye Friend!")
+  if message.content.startswith("!creator"):
+    await message.channel.send(
+        "I have been created by 'Bean Bag' aka ' mighty8005 '")
 
-  
-
+keep_alive()
 my_secret = os.environ['TOKEN']
 client.run(my_secret)
